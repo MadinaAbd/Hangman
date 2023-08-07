b042bfd05d264b454af77fab00690469f295d9af
Madina Abdurazakova <modi_ki@mail.ru>
2023-08-07 22:27:07 +0300

    Added gem colorize

---
+++ lib/console_interface.rb
@@ -0,0 +1,45 @@
+class ConsoleInterface
+
+  FIGURES =
+      Dir["#{__dir__}/../data/figures/*.txt"].
+      sort.
+      map { |file_name| File.read(file_name) }
+
+  def initialize(game)
+    @game = game
+  end
+
+  def print_out
+    puts <<~CURRENT_GAME_STATUS
+      Слово #{word_to_show.colorize(:violet)}
+      #{figure.colorize(color: :blue)}
+
+      Количество ошибок #{@game.errors_made}, осталось ошибок #{@game.errors_allowed}
+      Неправильные буквы: #{errors_to_show.colorize(:red)}
+
+    CURRENT_GAME_STATUS
+
+    if @game.won?
+      puts "Вы выиграли".colorize(:green)
+    elsif @game.lost?
+      puts "Вы проиграли, загаданное слово #{@game.word}".colorize(color: :red, :background => :light_green)
+    end
+  end
+
+  def figure
+    FIGURES[@game.errors_made]
+  end
+
+  def word_to_show
+    @game.letters_to_guess.map { |letter| letter || "__" }.join(" ")
+  end
+
+  def errors_to_show
+    @game.errors.join(", ")
+  end
+
+  def get_input
+    print "Введите следующую букву: ".colorize(color: :green, :background => :grey)
+    letter = gets[0].upcase
+  end
+end
